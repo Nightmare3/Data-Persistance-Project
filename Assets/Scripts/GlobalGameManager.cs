fb650afa7dcfba683a760d618adb6432859d5f40
KnightmareX <wesleyvelderman@hotmail.com>
2023-02-24 22:00:11 +0100

    Save/Load game
    Added save game and load game functionality

--- Assets/Scripts/GlobalGameManager.cs
+++ Assets/Scripts/GlobalGameManager.cs
@@ -1,18 +1,102 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
+
+[System.Serializable]
+public class PlayerData
+{
+    [SerializeField] private int score;
+    [SerializeField] private string username;
+
+    public PlayerData() { }
+    public PlayerData(string username, int score)
+    {
+        Username = username;
+        Score = score;
+    }
+    public int Score { get => score; set => score = value; }
+    public string Username { get => username; set => username = value; }
+}
+
+[System.Serializable]
+public class Scoreboard
+{
+    [SerializeField] private List<PlayerData> scoreboard;
+
+    public Scoreboard()
+    {
+        if (scoreboard == null)
+        {
+            scoreboard = new List<PlayerData>();
+        }
+    }
+    public void addPlayerData(PlayerData playerdata)
+    {
+        scoreboard.Add(playerdata);
+    }
+
+    public PlayerData GetTopPlayer()
+    {
+        if(scoreboard.Count > 0)
+        {
+            PlayerData topPlayer = new PlayerData("", 0);
+            foreach(PlayerData player in scoreboard)
+            {
+                if(player.Score > topPlayer.Score)
+                {
+                    topPlayer = player;
+                }
+            }
+            return topPlayer;
+        }
+        return null;
+    }
+}
+
+[System.Serializable]
+public class GameInformation
+{
+    [SerializeField] public PlayerData currentPlayerData;
+    [SerializeField] public Scoreboard scoreboard;
+}
 
 public class GlobalGameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static GlobalGameManager Instance;
+    public GameInformation gameInformation;
+
+    private void Awake()
     {
+        if (gameInformation == null)
+        {
+            gameInformation = new GameInformation();
+            LoadGame();
+        }
         
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SaveGame()
     {
-        
+        string jsonObject = JsonUtility.ToJson(gameInformation.scoreboard);
+        File.WriteAllText(Application.persistentDataPath + "/saveFile.json", jsonObject);
+    }
+
+    public void LoadGame()
+    {
+        string dataPath = Application.persistentDataPath + "/saveFile.json";
+        if (File.Exists(dataPath))
+        {
+            string jsonObject = File.ReadAllText(dataPath);
+            gameInformation.scoreboard = JsonUtility.FromJson<Scoreboard>(jsonObject);
+        }
     }
 }
